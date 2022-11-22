a3565a876f79499deee48c42d2a0bda18684c328
tucket33 <brazenstint@tutanota.com>
2022-11-22 14:10:49 -0500

    change copyright

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     const year = new Date()
 
   return  <footer className="flex flex-col justify-center items-center w-full bg-slate-100 bottom-0 h-8 fixed shadow-inner">
-            <p className="text-center">Copyright &copy; Abstract Web Dev {year.getFullYear()}</p>
+            <p className="text-center">Copyright &copy; Danny Asema {year.getFullYear()}</p>
           </footer>
 };
 
